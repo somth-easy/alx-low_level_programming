2527aba9467965bc3f6d2f6d168b90d7a55b76e3
somth-easy <somth_dope@yahoo.com>
2023-02-23 04:20:50 -0800

    Debbugin file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Checks to see if the integer is positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
