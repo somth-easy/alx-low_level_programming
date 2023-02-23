b69baaaa0442b7c8f40b8ccc9dd52b2fbe431409
Biko <123925445+somth-easy@users.noreply.github.com>
2023-02-24 01:15:10 +0200

    Print Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,13 @@ void print_triangle(int size)
 int row, hashes, spaces;
 
 if (size <= 0)
-        {
-_putchar('\n');
-        }
+{
+ _putchar('\n');
+}
 else
 {
 for (row = 1; row <= size; row++)
-                {
+{
 for (spaces = size - row; spaces >= 1; spaces--)
 {
 _putchar(' ');
