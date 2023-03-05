e7e32d169be7fb671bb545515dbd01f208224d6e
Biko <123925445+somth-easy@users.noreply.github.com>
2023-03-06 00:11:21 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int y;
-	int t;
-
-	s = 0;
-	while (dest[s] != '\0')
-	{
-		s++;
-	}
-	t = 0;
-	while (src[t] != '\0')
-	{
-		dest[s] = src[t];
-		s++;
-		t++;
-	}
-
-	dest[s] = '\0';
-	return (dest);
+char *s = dest;
+while (*dest) {
+dest++;
+}
+while ((*dest++ = *src++)) {}
+return s;
 }
