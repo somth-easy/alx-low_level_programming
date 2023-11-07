423ef1ded07c76003dbcec6672a3d5f2afe6365f
Biko <123925445+somth-easy@users.noreply.github.com>
2023-11-07 21:29:13 +0200

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Prints with write function
- *
- * Return: 1 and exit
- */
-
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
