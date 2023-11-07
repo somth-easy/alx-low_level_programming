8893100e90c60e872fa1ced85cab9a7e1231516b
somth-easy <somth_dope@yahoo.com>
2023-11-07 14:40:59 +0000

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main: Prints with write function
+ *
+ * Return: 1 Always
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+
+	return (1);
+}
