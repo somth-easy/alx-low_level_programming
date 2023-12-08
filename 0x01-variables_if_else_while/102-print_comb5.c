bb91aa5eb5e1e114fe7f839c0a3b367b276dcc4b
Biko <123925445+somth-easy@users.noreply.github.com>
2023-12-08 18:49:46 +0200

    variables if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
-
-/**
- * main - Entry Point
- *
- * Return: Always (0)
- */
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	int a, b, c, d;
+        int a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
-	{
-		for (b = '0'; b <= '9'; b++)
-		{
-			for (c = (a == '1' && b == '9') ? a + 1 : '0'; c <= '9'; c++)
-			{
-				for (d = (a == '1' && c == '0' && b == '9') ? b + 1 : '0'; d <= '9'; d++)
-				{
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(c);
-					putchar(d);
-
-					if (!(a == '9' && b == '8' && c == '9' && d == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        d = c = b = a = '0';
+        while (a <= '9')
+        {
+                if (a >= '1')
+                {
+                        b = '0';
+                        c = a;
+                        d = b + 1;
+                }
+                while (b <= '9')
+                {
+                        if (b >= '1' &&  b < '9' && a < '1')
+                        {
+                                c = a;
+                                d = b + 1;
+                        }
+                        if (b == '9' && a < '1')
+                        {
+                                c = a + 1;
+                                d = '0';
+                        }
+                        if (a >= '1' && b >= '1')
+                        {
+                                c = a;
+                                d = b + 1;
+                        }
+                        while (c <= '9')
+                        {
+                                if (c >= '1' && c != a)
+                                        d = '0';
+                                while (d <= '9')
+                                {
+                                        putchar(a);
+                                        putchar(b);
+                                        putchar(' ');
+                                        putchar(c);
+                                        putchar(d);
+                                        if (a == '9' && b == '8' && c == '9' && d == '9')
+                                                break;
+                                        putchar(',');
+                                        putchar(' ');
+                                        ++d;
+                                } ++c;
+                        } ++b;
+                } ++a;
+        } putchar('\n');
+        return (0);
 }
