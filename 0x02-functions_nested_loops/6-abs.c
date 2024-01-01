b8c523d92339eeda76e519b4318acd7db9d439a3
somth-easy <somth_dope@yahoo.com>
2024-01-01 15:02:31 +0000

    Functions and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @int: data type passed to the function
+ *
+ * Return: The absolute value
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
