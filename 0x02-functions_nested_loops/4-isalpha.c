7e0e354e909500f4a9374d1c6bb9284514274e4f
somth-easy <somth_dope@yahoo.com>
2024-01-01 13:39:18 +0000

    Functions and Loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: hold copy of value parsed to function
+ * Return; on success
+ */
+
+int _isalpha(int c)
+{
+	char start_lower = 'a', end_lower = 'z';
+	char current = start_lower;
+
+	while (current <= end_lower)
+	{
+		if ((current == c) || ((current - 'a' + 'A') == c))
+		{
+			return (1);
+		}
+		current++;
+	}
+	return (0);
+}
