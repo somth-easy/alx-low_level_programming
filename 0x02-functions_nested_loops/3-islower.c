bc97d07762fbc68f4dc592ad456919c5a0381384
somth-easy <somth_dope@yahoo.com>
2024-01-01 10:10:17 +0000

    Functions and Lopps

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: on success
+ */
+
+int _islower(int c)
+{
+	char current, start = 'a', end = 'z';
+
+	for(current = start; current <= end ; current++)
+	{
+		if (current == c)
+		{
+		
+			return (1);
+		}
+	}
+	return (0);
+}
