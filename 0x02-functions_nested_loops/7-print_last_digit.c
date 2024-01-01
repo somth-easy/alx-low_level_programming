fedf048aaa7d03da74283938c0d089fc2f1e56a7
somth-easy <somth_dope@yahoo.com>
2024-01-01 15:14:24 +0000

    Functions and Loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: holds copy of the value parsed to function
+ *
+ * Return: on success
+ */
+
+int print_last_digit(int n)
+{
+	int i, retval;
+
+	i = 10;
+
+	retval = n % i;
+
+	return (retval);
+}
