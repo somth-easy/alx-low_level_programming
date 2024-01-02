95cb5df6a4b7d8a4e4232ffd488652004dc6c4f8
somth-easy <somth_dope@yahoo.com>
2024-01-02 10:53:50 +0000

    Functions and loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ * @a: first copy of parsed int value
+ * @b: second copy of a parsed int value
+ *
+ * Return: on success
+ */
+
+int add(int a, int b)
+{
+	int retval = a + b;
+
+	return (retval);
+}
+
