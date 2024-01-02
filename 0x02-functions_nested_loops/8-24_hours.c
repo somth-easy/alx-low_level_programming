6e70210f7f3666453ddb047cb0c48e73e837377b
somth-easy <somth_dope@yahoo.com>
2024-01-02 09:19:35 +0000

    funcitons and loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: on success
+ */
+
+int jack_bauer(void)
+{
+	int i, n;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (n = 0; n <= 59; n++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
