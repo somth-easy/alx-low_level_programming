dfd587541e1d6bfbd5e32e9d882e14104e1e5539
somth-easy <somth_dope@yahoo.com>
2024-01-02 15:28:22 +0000

    Functions and Loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,65 +1,25 @@
 #include "main.h"
 
 /**
- * print_to_98 - Entry point
+ * print_to_98 - entry point
  *
  * @n: hold copy of parsed value
  *
  * Return: on success
  */
 
+void over_98(int n);
+void under_0(int n);
+
+
 void print_to_98(int n)
 {
-	int i, m, x;
+	int i;
 
 	i = n;
-	x = 1;
 
-	if (i > 98)
-	{
-		for (i = n; i > 98; i--)
-		{
-			if(i > 100)
-			{
-				_putchar(x +'0');
-				m = i - 100;
-				_putchar((m / 10) + '0');
-				_putchar((m % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i < 100)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
-	}
-
-	if (i < 0)
-	{
-		m = i * -1;
-
-		while ( m > 0)
-		{
-			_putchar('-');
-			if (m > 9)
-			{
-				_putchar((m / 10) + '0');
-			}
-			_putchar((m % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			m--;
-			i++;
-		}
-
-	}
+	over_98(i);
+	under_0(i);
 
 	if (i >= 0 && i <= 98)
 	{
@@ -84,4 +44,83 @@ void print_to_98(int n)
 	}
 	_putchar('\n');
 
+}
+
+/**
+ * over_98 - entry point
+ *
+ * @n: value copy
+ *
+ * Return: on success
+ */
+
+void over_98(int n)
+{
+	int i, x, m;
+
+	i = n;
+	x = 1;
+
+	if (i > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			if (i > 100)
+			{
+				_putchar(x + '0');
+				m = i - 100;
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (i < 100)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+
+				}
+			}
+		}
+	}
+}
+
+/**
+ * under_0 - entry point
+ *
+ * @n: value copy
+ *
+ * Return: on success
+ */
+
+void under_0(int n)
+{
+	int i, m;
+
+	i = n;
+
+	if (i < 0)
+	{
+		m = i * -1;
+
+		while (m > 0)
+		{
+			_putchar('-');
+
+			if (m > 9)
+			{
+				_putchar((m / 10) + '0');
+			}
+			_putchar((m % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			m--;
+			i++;
+		}
+	}
 }
