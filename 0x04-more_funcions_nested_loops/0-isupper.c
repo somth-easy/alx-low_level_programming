d6a64b1b0a0fdf8777752d7ee933aced4657dc70
somth-easy <somth_dope@yahoo.com>
2024-01-03 15:24:35 +0000

    Funcitons and Loops

---
+++ 0x04-more_funcions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - entry point
+ * @c: passed value
+ * Return: on success
+ */
+
+int _isupper(char c)
+{
+	char start = 'A', end = 'Z';
+	int i;
+
+	for (i = start; i <= end; i++)
+	{
+		if (i == c)
+		{
+			return(1);
+		}
+	}
+	return (0);
+}
