baa5e5001198d4c9cd013e681cbf0468601202fd
somth-easy <somth_dope@yahoo.com>
2024-01-04 13:20:31 +0000

    Pointers Strings Arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ * @str: points to value in address passed
+ * Return: on success
+ */
+
+void rev_string(char *str)
+{
+	int i, m, size = 0;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		size++;
+		ptr++;
+	}
+
+	size -= 1;
+	i = size;
+
+	while (i > 0)
+	{
+		ptr--;
+		_putchar(*ptr);
+		if (i == 0)
+		{
+			_putchar('\n');
+		}
+		i--;
+	}
+	ptr = '\0';
+}
