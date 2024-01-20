de7dcbb1ea9110f670e8b8a0ef6d5f5cbb444975
somth-easy <somth_dope@yahoo.com>
2024-01-20 19:26:45 +0000

    pointers arrays strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+
+	while ((s1[i] != '\0') && (s2[i] != '\0') && (s1[i] == s2[i]))
+	{
+		s1++;
+		s2++;
+	}
+
 	if (*s1 == *s2)
 	{
 		return (0);
 	}
 	else
 	{
-		return(*s1 - *s2);
+		return (*s1 - *s2);
 	}
 }
