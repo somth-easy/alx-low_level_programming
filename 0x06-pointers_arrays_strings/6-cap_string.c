ad3d80b1e5977891063cb38be4a9055f8010b8f8
somth-easy <somth_dope@yahoo.com>
2024-01-21 14:02:48 +0000

    pointers arrays strins

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - entry point
+ * @c: char pointer variable
+ * Return: on success
+ */
+
+char *cap_string(char *c)
+{
+	int i, b = 0;
+
+	char d[] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '\"', '(', ')', '{', '}', '\0'};
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (b = 0; d[b] != '\0'; b++)
+		{
+			if (c[i - 1] == d[b] && c[i] >= 'a' && c[i] <= 'z')
+			{
+				c[i] = c[i] - 'a' + 'A';
+			}
+		}
+	}
+
+	return (c);
+}
+
+
