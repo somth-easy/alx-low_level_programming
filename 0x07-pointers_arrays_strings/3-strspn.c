d05b08816d817999c84251bdaf18b54048a6bdfb
somth-easy <somth_dope@yahoo.com>
2024-01-24 11:23:19 +0000

    pointers arrays strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,9 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, n, y, z;
+	int i, n = 0, y = 0, z = 0;
 	unsigned int x = 0;
-        char tmp[50];
-	
-	n = 0;
-	y = 0;
-	z = 0;
+	char tmp[50];
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -23,45 +19,38 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			while (accept[z] != s[i])
 			{
-				if(accept[z] == '\0')
-				{
+				if (accept[z] == '\0')
 					goto skip;
-				}
 				z++;
 			}
 			tmp[y] = s[i];
 			y++;
 			tmp[y] = '\0';
-			n++;
-			x++;
+			n++; x++;
 		}
 skip:
 		z = 0;
-
-		while (tmp[y]!= '\0')
+		while (tmp[y] != '\0')
 		{
 			while (accept[z] != s[i])
-                        {
-                                if(accept[z] == '\0')
-                                {
-                                        goto jump;
-                                }
-                                z++;
-                        }
-
-			if ((tmp[y] == s[i]) && tmp[y + 1] != '\0')
+			{
+				if (accept[z] == '\0')
+				{
+					goto jump;
+				}
+				z++;
+			}
+			if ((tmp[y] == s[i]) && (tmp[y + 1] != '\0' || tmp[y + 1] == '\0'))
 			{
 				goto jump;
 			}
 			y++;
-
-			if(tmp[y] == '\0') 
+			if (tmp[y] == '\0')
 			{
 				tmp[y] = s[i];
 				y++;
 				tmp[y] = '\0';
-				n++;
-				x++;
+				n++; x++;
 			}
 
 		}
@@ -69,5 +58,6 @@ jump:
 		y = 0;
 
 	}
+	x++;
 	return (x);
 }
