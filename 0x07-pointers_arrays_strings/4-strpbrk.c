237f6844a89940652b5a244cecc5db719f642e4b
somth-easy <somth_dope@yahoo.com>
2024-01-25 11:56:00 +0000

    pointer arrays strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - entry point
+ * @s: char pointer variable
+ * @accept: char pointer variable
+ * Return: on success
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n = 0;
+
+	while (s[n] != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[n] == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (s);
+}
