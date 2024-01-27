1e895ae162ee89cfd9e4696e8167e5ccd76a4f44
somth-easy <somth_dope@yahoo.com>
2024-01-27 18:04:43 +0000

    pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - entry point
+ * @haystack: char pointer variable
+ * @needle: char pointer variable
+ * Return: on success
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		i = 0;
+		while ((haystack[i] == needle[i]) && (needle[i] != '\0'))
+		{
+			if(haystack[i] != needle[i])
+				break;
+			if (needle[i + 1] == '\0')
+				return (haystack);
+			i++;
+		}
+		haystack++;
+	}
+	return (NULL);
+}
