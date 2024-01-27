8d213b9de40054336fe30b0f0bd29f37536fd323
somth-easy <somth_dope@yahoo.com>
2024-01-27 18:32:58 +0000

    pointers arrays strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
-	if (*needle == '\0')
-		return (NULL);
-
 	while (*haystack)
 	{
 		i = 0;
 		while ((haystack[i] == needle[i]) && (needle[i] != '\0'))
 		{
-			if (haystack[i] != needle[i])
-				break;
 			if (needle[i + 1] == '\0')
 				return (haystack);
 			i++;
