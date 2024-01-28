15a4280ff9ea12787f0866606e43f66e27c43f5e
somth-easy <somth_dope@yahoo.com>
2024-01-28 07:38:16 +0000

    pointers arrays strings)

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: char pointer to multi dimencional array
+ * Return: on success
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			putchar(a[i][n]);
+		}
+		putchar ('\n');
+	}
+}
