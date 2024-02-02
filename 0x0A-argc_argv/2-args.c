c34f562b897d8821a78911876c25db60b9a1c3fa
somth-easy <somth_dope@yahoo.com>
2024-02-02 16:00:16 +0000

    Argc Argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,33 +10,7 @@
 int main(int argc, char *argv[])
 {
 	while (argc--)
-		_puts(*argv++);
+		puts(*argv++);
 
 	return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _puts - prints a string,followed by a new line, to stdout
- * @str: string to print
- */
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-	_putchar('\n');
 }
