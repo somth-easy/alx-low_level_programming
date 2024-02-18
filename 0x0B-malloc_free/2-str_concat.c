3bd965ed9ac72c69a7a9508d31960e2fc4dbcedd
somth-easy <somth_dope@yahoo.com>
2024-02-18 12:32:15 +0000

    Dynamic memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	size_t size1, size2, len;
 	unsigned int i, n;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
 	for (size1 = 0; s1[size1] != '\0'; size1++)
