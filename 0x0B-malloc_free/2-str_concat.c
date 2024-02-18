24065863bab831edfe45ccf3472690a7a197a2b8
somth-easy <somth_dope@yahoo.com>
2024-02-18 12:30:55 +0000

    Dynamic memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,30 +10,26 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	size_t len, len2;
+	size_t size1, size2, len;
 	unsigned int i, n;
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	for (len = 0; s1[len] != '\0'; len++)
+	for (size1 = 0; s1[size1] != '\0'; size1++)
 		;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		;
+	len = size1 + size2 + 1;
 
-	ptr = malloc(sizeof(char) * (len + 1));
+	ptr = malloc(sizeof(char) * len);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < size1; i++)
 		ptr[i] = s1[i];
 
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-
-	ptr = realloc(ptr, sizeof(char) * (len + len2 + 1));
-	if (ptr == NULL)
-		return (NULL);
-
-	for (n = 0; n < len2; n++, i++)
+	for (n = 0; n < size2; n++, i++)
 		ptr[i] = s2[n];
 
 	ptr[i] = '\0';
