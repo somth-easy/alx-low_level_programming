69188e933686fc1e9b52d37b91904b002598625a
somth-easy <somth_dope@yahoo.com>
2024-02-27 10:10:16 +0000

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#define FUNCITON_LIKE_MACRO_H
 #ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < (0) ? -(x):(x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif /*FUNCTION_LIKE_MACRO_H*/
