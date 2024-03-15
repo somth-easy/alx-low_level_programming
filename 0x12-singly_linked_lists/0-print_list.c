5d001229a1731c4809ee115bb36696ded4a19e66
somth-easy <somth_dope@yahoo.com>
2024-03-15 22:22:27 +0200

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,13 +18,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		x++;
 		h = h->next;
 	}
