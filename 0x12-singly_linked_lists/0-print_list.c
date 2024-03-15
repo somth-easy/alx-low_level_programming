f142e7f5988879c46025f22604fb93f3275dce91
somth-easy <somth_dope@yahoo.com>
2024-03-15 22:15:05 +0200

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,29 +1,32 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include "lists.h"
 
 /**
- * print_list - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
+ * print_list - entry point
+ * @h: node pointer
+ *
+ * Return: on success
  */
+
 size_t print_list(const list_t *h)
 {
-        int count = 0;
+	int x = 0;
 
-        while (h)
-        {
-                if (h->str == NULL)
-                {
-                        printf("[0] (nil)\n");
-                }
-                else
-                {
-                printf("[%d] %s\n", h->len, h->str);
-                }
-                count++;
-                h = h->next;
-        }
-        return (count);
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d]  %s\n", h->len, h->str);
+		}
+		x++;
+		h = h->next;
+	}
+	return (x);
 }
