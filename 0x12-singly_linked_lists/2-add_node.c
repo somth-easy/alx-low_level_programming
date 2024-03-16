e7e72942e78b8fc935d45462eb9b48474f5bf40c
somth-easy <somth_dope@yahoo.com>
2024-03-16 10:11:00 +0200

    add new head node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_node - entry point
+ *
+ * @head: head pointer
+ * @str: member pointer
+ *
+ * Return: on success
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t len;
+	list_t *new_head = (list_t *)malloc(sizeof(list_t));
+
+	new_head->str = strdup(str);
+	new_head->next = *head;
+	new_head->len = strlen(str);
+	*head = new_head;
+
+	return (new_head);
+}
+
+
