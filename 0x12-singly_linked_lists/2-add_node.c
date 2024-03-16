83577cabf2a02ad6a012bf3e45b6d1f8300d6b1a
somth-easy <somth_dope@yahoo.com>
2024-03-16 10:26:05 +0200

    add new head node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	iist_t *new_head = (list_t *)malloc(sizeof(list_t));
-	if (new_head = NULL)
+	if (new_head == NULL)
 		return (NULL);
 
 	new_head->str = strdup(str);
